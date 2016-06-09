42ff3e74302f50d7f605fb14a24ff3e1c5ca3024
Ruben Rodriguez Malvis <RRM@DETIN.CAC>
2016-06-09 16:58:15 -0300

    hibernate e reverse implementados

---
+++ src/main/java/br/com/test/testaJPA.java
@@ -0,0 +1,40 @@
+package br.com.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.pojos.Processo;
+import br.com.pojos.ProcessoId;
+import br.com.pojos.SubProcesso;
+
+public class testaJPA {
+	
+	public static void main(String[] args) {
+		
+		pegaSubProcessoPeloID();
+		
+		System.out.println("FIM");
+		
+	}
+	
+	public static void pegaSubProcessoPeloID(){
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("reverse-jpa");
+		EntityManager manager = factory.createEntityManager();
+				
+		ProcessoId processoId = new ProcessoId(2259, new Short("2016"), 12);
+		
+		Processo processo = manager.find(Processo.class, processoId);
+		
+		System.out.println(processo);
+		
+		for (SubProcesso subProcesso : processo.getSubProcessos()) {
+			System.out.println(subProcesso.getId());
+		}
+
+		manager.close();
+		
+	}
+
+}
